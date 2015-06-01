3d048dcaf50c83ed333c00b17c0a5cc51da07925
Dao Minh Duc <brokenheart1712@gmail.com>
2015-06-02 03:08:55 +0700

    Add Review Model And Relationship

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
+  
+  validates :user_id, presence: true
+  validates :book_id, presence: true
+  validates :content, presence: true
+  validates :rating, presence: true
 end
