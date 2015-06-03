363fe96dc9c1196beb6f523c1f9dbd220e8d0449
Dao Minh Duc <daominhduc@Xubuntu-14.04.2>
2015-06-03 10:05:33 +0700

    Show Books For Normal Users

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -2,7 +2,9 @@ class Admin::BooksController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @books = @books.paginate page: params[:page], per_page: Settings.books_per_page_size
+    @books = @books.order_by_created_at
+                   .paginate page: params[:page],
+                             per_page: Settings.books_per_page_size
   end
 
   def new
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,8 @@
 class Book < ActiveRecord::Base
   belongs_to :category
-  
+
   has_many :reviews, dependent: :destroy
   has_many :perusals, dependent: :destroy
+
+  scope :order_by_created_at, -> {order created_at: :DESC}
 end
