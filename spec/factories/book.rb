663c25abb33715e7e90eb8dedc9716530398f5c0
Nhat Tran Van <tran.van.nhat@framgia.com>
2015-06-12 11:28:47 +0700

    fix header and modify book.rb file

--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -3,6 +3,8 @@ FactoryGirl.define do
     title {Faker::Lorem.sentence(3, true, 3)}
     author {Faker::Name.name}
     ISBN {Faker::Number.number(10)}
+    number_of_pages {Faker::Number.number(3)}
+    publication_date {Faker::Date.backward(14)}
     rating {1 + rand(5)}
     description {Faker::Lorem.paragraph(1, true, 6)}
     picture {File.new("#{Rails.root}/spec/photos/#{1+rand(12)}.jpeg")}
